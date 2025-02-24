1064f759da8a4e37d90f432b261fb4bc7b2f866d
Gabriel Zantua <138269636+gabrielzantua@users.noreply.github.com>
2025-02-24 23:11:43 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/PROJECT-R.I.S.E",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
